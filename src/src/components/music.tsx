b68188adf8c1ae16c1d44a606f53fac43815d16c
ezefferth <ezefferth96@gmail.com>
2025-02-16 09:39:38 -0400

    shadow

--- src/src/components/music.tsx
+++ src/src/components/music.tsx
@@ -2,7 +2,7 @@ import musicFile from "../../assets/riverFlowsinYou.mp3"; // 🔥 Adicione um ar
 
 export default function Music() {
   return (
-    <div className="fixed bottom-8 left-0 w-full flex justify-center shadow-md z-50">
+    <div className="fixed bottom-8 left-0 w-full flex justify-center z-50">
       <audio controls autoPlay loop className="w-[80%] max-w-lg">
         <source src={musicFile} type="audio/mp3" />
       </audio>
